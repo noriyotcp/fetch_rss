57d9033b94848ff641e4061772d6a4548ab0847d
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-15 15:38:36 +0000

    Enable to add parameters on top

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,8 @@ class TopController < ApplicationController
     @hatebu = Hatebu.new
     @hnrss = Hnrss.new
 
-    hatebu_posts = @hatebu.fetch
-    hnrss_posts = @hnrss.fetch
+    hatebu_posts = @hatebu.fetch(params[:tag], params[:sort])
+    hnrss_posts = @hnrss.fetch(params[:tag], params[:points])
 
     feeds = hatebu_posts.concat(hnrss_posts)
 
