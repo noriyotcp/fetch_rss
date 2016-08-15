ea2eb03b79f312f2c37f99990ab1560dd78aadf5
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-15 12:57:02 +0000

    [controllers] Big refactoring using models

--- app/controllers/hnrss_controller.rb
+++ app/controllers/hnrss_controller.rb
@@ -1,37 +1,11 @@
 class HnrssController < ApplicationController
-  require 'rss'
-  require 'open-uri'
-
-  BASE_URL = 'http://hnrss.org/newest'
-  TAGS = ['Rails', 'Swift']
-
   def fetch
-    tags = Array.new
-    tags = params[:tag].blank? ? TAGS : tags.push(params[:tag])
-
-    sort_param = params[:points].present? ? "&points=#{params[:points]}" : ""
-
-    urls = tags.map { |tag| create_url_by_tag(tag) }
-    feeds = urls.map { |url| fetch_feed(url + sort_param) }
+    @hnrss = Hnrss.new
+    feeds = @hnrss.fetch(params[:tag], params[:points])
     render json: feeds.flatten.sort! { |a,b| b[:date] <=> a[:date] }
   end
 
   private
-
-  def fetch_feed(url)
-    @url = url
-    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-    rss = open(@url, 'User-Agent' => ua)
-    feed = RSS::Parser.parse(rss)
-    rss.close
-    return feed.items.map do |i|
-      { link: i.link, title: i.title, desc: i.description, date: i.pubDate }
-    end
-  end
-
-  def create_url_by_tag(tag)
-    return "#{BASE_URL}?q=#{tag}"
-  end
 
   def hnrss_params
     params.permit(:tag, :points)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,38 +1,11 @@
 class PostsController < ApplicationController
-  require 'rss'
-  require 'open-uri'
-
-  BASE_URL = 'http://b.hatena.ne.jp/search/tag'
-  TAGS = ['Rails', 'Swift']
-
   def fetch
-    tags = Array.new
-    tags = params[:tag].blank? ? TAGS : tags.push(params[:tag])
-
-    sort_param = params[:sort].present? ? "&sort=#{params[:sort]}" : ""
-
-    urls = tags.map { |tag| create_url_by_tag(tag) }
-
-    feeds = urls.map { |url| fetch_feed(url + sort_param) }
+    @post = Post.new
+    feeds = @post.fetch(params[:tag], params[:sort])
     render json: feeds.flatten.sort! { |a,b| b[:date] <=> a[:date] }
   end
 
   private
-
-  def fetch_feed(url)
-    @url = url
-    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-    rss = open(@url, 'User-Agent' => ua)
-    feed = RSS::Parser.parse(rss)
-    rss.close
-    return feed.items.map do |i|
-      { link: i.link, title: i.title, desc: i.description, date: i.dc_date }
-    end
-  end
-
-  def create_url_by_tag(tag)
-    return "#{BASE_URL}?q=#{tag}&mode=rss"
-  end
 
   def post_params
     params.permit(:tag, :sort)
