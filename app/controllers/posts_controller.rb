116cf244eb4d89248c3b5e168491599be3ae3228
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-15 10:46:58 +0000

    Fetch by parameters on PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,26 +2,19 @@ class PostsController < ApplicationController
   require 'rss'
   require 'open-uri'
 
-  # before_action :fetch_feed, only: [:fetch]
   BASE_URL = 'http://b.hatena.ne.jp/search/tag'
   TAGS = ['Rails', 'Swift']
   SORT_BY = { recent: 'recent', popular: 'popular' }
 
   def fetch
-    urls = TAGS.map { |tag| create_url_by_tag(tag) }
-    feeds = urls.map { |url| fetch_feed(url) }
-    render json: feeds.flatten.sort! { |a,b| b[:date] <=> a[:date] }
-  end
+    tags = Array.new
+    tags = params[:tag].blank? ? TAGS : tags.push(params[:tag])
 
-  def fetch_by_recent
-    urls = TAGS.map { |tag| create_url_by_tag(tag) }
-    feeds = urls.map { |url| fetch_feed(url+"&sort=#{SORT_BY[:recent]}") }
-    render json: feeds.flatten.sort! { |a,b| b[:date] <=> a[:date] }
-  end
+    sort_param = params[:sort].present? ? "&sort=#{params[:sort]}" : ""
 
-  def fetch_by_popular
-    urls = TAGS.map { |tag| create_url_by_tag(tag) }
-    feeds = urls.map { |url| fetch_feed(url+"&sort=#{SORT_BY[:popular]}") }
+    urls = tags.map { |tag| create_url_by_tag(tag) }
+
+    feeds = urls.map { |url| fetch_feed(url + sort_param) }
     render json: feeds.flatten.sort! { |a,b| b[:date] <=> a[:date] }
   end
 
@@ -29,7 +22,6 @@ class PostsController < ApplicationController
 
   def fetch_feed(url)
     @url = url
-    # url = 'http://b.hatena.ne.jp/search/tag?q=Rails&mode=rss&sort=recent'
     ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
     rss = open(@url, 'User-Agent' => ua)
     feed = RSS::Parser.parse(rss)
@@ -41,5 +33,9 @@ class PostsController < ApplicationController
 
   def create_url_by_tag(tag)
     return "#{BASE_URL}?q=#{tag}&mode=rss"
+  end
+
+  def post_params
+    params.permit(:tag, :sort)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get '/fetch', to: 'posts#fetch'
-  get '/recent', to: 'posts#fetch_by_recent'
-  get '/popular', to: 'posts#fetch_by_popular'
 
   get 'hnrss/fetch', to: 'hnrss#fetch'
   get 'hnrss/recent', to: 'hnrss#fetch_by_recent'
