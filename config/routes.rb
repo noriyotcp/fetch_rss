7edd83279126a3e33e846a70421b98f7a7804dc3
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-15 13:14:12 +0000

    Remove post(s)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'hatebu/fetch', to: 'hatebu#fetch'
-
-  root :to => 'top#index'
-  get '/fetch', to: 'posts#fetch'
   get 'hnrss/fetch', to: 'hnrss#fetch'
+  root :to => 'top#index'
 end
